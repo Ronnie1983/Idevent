7bda2f6e76eaf0d3b79d13a9a6ab69b44e1caf6a
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2021-11-09 13:50:27 +0100

    Added some tables to template. (still needs updating).

---
+++ IdeventSQLServerTemplate/dbo/Tables/EventStands.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[EventStands]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [FK_EventId] INT NOT NULL, 
+    [FK_EventStandFunctionalityId] INT NOT NULL
+)
---
+++ IdeventSQLServerTemplate/dbo/Tables/Events.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Events]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [FK_CompanyId] INT NOT NULL
+)
