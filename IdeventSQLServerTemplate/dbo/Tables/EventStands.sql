c682c667dc92962ddc7bbfac34d9078239c98a90
Thomas (Noit) <tba@live.dk>
2021-11-10 11:55:09 +0100

    Updated EventStands naming. Added Adressess table.

--- IdeventSQLServerTemplate/dbo/Tables/EventStands.sql
+++ IdeventSQLServerTemplate/dbo/Tables/EventStands.sql
@@ -3,5 +3,5 @@
 	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
     [Name] NVARCHAR(50) NOT NULL, 
     [FK_EventId] INT NOT NULL, 
-    [FK_EventStandFunctionalityId] INT NOT NULL
+    [FK_StandFunctionalityId] INT NOT NULL
 )
