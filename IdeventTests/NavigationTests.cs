89eaacfe3bf2f32f5fb0eca0f49c2d084cf91b6a
Thomas (Noit) <tba@live.dk>
2021-11-11 13:22:44 +0100

    Wrote Dashboard test.

--- IdeventTests/NavigationTests.cs
+++ IdeventTests/NavigationTests.cs
@@ -1,4 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bunit;
+using IdeventAdminBlazorServer.Pages;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components;
 
 namespace IdeventTests
 {
@@ -6,10 +10,53 @@ namespace IdeventTests
     [TestClass]
     public class NavigationTests
     {
-        [TestMethod]
-        public void NavigateToDashboard()
+        private Bunit.TestContext _testContext;
+        private NavigationManager _navManager;
+
+        [TestInitialize]
+        public void Inititialise()
         {
+            _testContext = new Bunit.TestContext();
+            _navManager = _testContext.Services.GetService<NavigationManager>();
+        }
+        [TestMethod]
+        public void DashboardIndex_Loads()
+        {
+            // Arrange
+            var dashboardPage = _testContext.RenderComponent<IdeventAdminBlazorServer.Pages.Admin.Dashboard.Index>();
             
+            string url = $"{_navManager.BaseUri}Home";
+            string expectedH1Tag = "<h1>Dashboard</h1>";
+
+            // Act
+            _navManager.NavigateTo(url);
+            bool containsExpected = dashboardPage.Markup.Contains(expectedH1Tag);
+
+            // Assert
+            Assert.AreEqual("http://localhost/Home", url);
+            Assert.IsTrue(containsExpected);
+        }
+        [TestMethod]
+        public void ProfileIndex_Loads()
+        {
+            // Arrange
+            var profilePage = _testContext.RenderComponent<IdeventAdminBlazorServer.Pages.Admin.Profile.Index>();
+
+            string url = $"{_navManager.BaseUri}Home";
+            string expectedH1Tag = "<h1>Profile</h1>";
+
+            // Act
+            _navManager.NavigateTo(url);
+            bool containsExpected = profilePage.Markup.Contains(expectedH1Tag);
+
+            // Assert
+            Assert.AreEqual("http://localhost/Home", url);
+            Assert.IsTrue(containsExpected);
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _testContext.Dispose();
         }
     }
 }
