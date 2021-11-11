e7c9c10043ffc1833b8e5559a291131e98a62630
Thomas (Noit) <tba@live.dk>
2021-11-11 13:47:44 +0100

    Added test for profile.

--- IdeventTests/NavigationTests.cs
+++ IdeventTests/NavigationTests.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bunit;
 using IdeventAdminBlazorServer.Pages;
-using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components;
 
 namespace IdeventTests
@@ -19,12 +18,17 @@ namespace IdeventTests
             _testContext = new Bunit.TestContext();
             _navManager = _testContext.Services.GetService<NavigationManager>();
         }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _testContext.Dispose();
+        }
         [TestMethod]
         public void DashboardIndex_Loads()
         {
             // Arrange
             var dashboardPage = _testContext.RenderComponent<IdeventAdminBlazorServer.Pages.Admin.Dashboard.Index>();
-            
+
             string url = $"{_navManager.BaseUri}Home";
             string expectedH1Tag = "<h1>Dashboard</h1>";
 
@@ -39,24 +43,32 @@ namespace IdeventTests
         [TestMethod]
         public void ProfileIndex_Loads()
         {
-            // Arrange
-            var profilePage = _testContext.RenderComponent<IdeventAdminBlazorServer.Pages.Admin.Profile.Index>();
+            LoadPageTest(_testContext.RenderComponent<IdeventAdminBlazorServer.Pages.Admin.Profile.Index>(), "Profile");
+        }
 
-            string url = $"{_navManager.BaseUri}Home";
-            string expectedH1Tag = "<h1>Profile</h1>";
+        private void LoadPageTest(IRenderedComponent<IComponent> pageComponent, string pageTitle, string navigationTitle = "")
+        {
+            // Arrange
+            var page = pageComponent;
+            string url;
+            if (string.IsNullOrEmpty(navigationTitle))
+            {
+                url = $"{_navManager.BaseUri}{pageTitle}";
+            }
+            else
+            {
+                url = $"{_navManager.BaseUri}{navigationTitle}";
+            }
+            string expectedH1Tag = $"<h1>{pageTitle}</h1>";
 
             // Act
             _navManager.NavigateTo(url);
-            bool containsExpected = profilePage.Markup.Contains(expectedH1Tag);
+
+            bool containsExpected = page.Markup.Contains(expectedH1Tag);
 
             // Assert
-            Assert.AreEqual("http://localhost/Home", url);
             Assert.IsTrue(containsExpected);
         }
-        [TestCleanup]
-        public void Cleanup()
-        {
-            _testContext.Dispose();
-        }
+
     }
 }
