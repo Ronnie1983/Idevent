391f61ac814012133b851268089a14368ca5a43b
Thomas (Noit) <tba@live.dk>
2021-11-12 16:25:29 +0100

    Added event model.

---
+++ IdeventLibrary/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeventLibrary.Models
+{
+    public class Event
+    {
+        public Event(){}
+
+        public Event(int id, string name, int companyId)
+        {
+            Id = id;
+            Name = name;
+            CompanyId = companyId;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
