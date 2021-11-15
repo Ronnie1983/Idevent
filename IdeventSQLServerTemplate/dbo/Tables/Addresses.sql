67555ebb46686820df25b9cf9d48221a324d7792
Thomas (Noit) <tba@live.dk>
2021-11-15 13:08:56 +0100

    Made a basic Insert script for Addresses, Companies and Events. Made Addresses ID Identity.

--- IdeventSQLServerTemplate/dbo/Tables/Addresses.sql
+++ IdeventSQLServerTemplate/dbo/Tables/Addresses.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Addresses]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
     [StreetAddress] NVARCHAR(100) NOT NULL, 
     [City] NVARCHAR(50) NOT NULL, 
     [Country] NVARCHAR(50) NOT NULL, 
