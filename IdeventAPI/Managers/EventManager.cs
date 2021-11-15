9c5d52110da927335164059b510fb685aefc1087
Thomas (Noit) <tba@live.dk>
2021-11-15 11:32:59 +0100

    Added a stored procedure for getting ALL events.

--- IdeventAPI/Managers/EventManager.cs
+++ IdeventAPI/Managers/EventManager.cs
@@ -1,6 +1,14 @@
-﻿namespace IdeventAPI.Managers
+﻿using IdeventLibrary.Models;
+using System;
+using System.Collections.Generic;
+
+namespace IdeventAPI.Managers
 {
     public class EventManager
     {
+        public List<EventModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ IdeventSQLServerTemplate/dbo/StoredProcedures/spGetAllEvents.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spGetAllEvents]
+AS
+BEGIN
+
+	SELECT Events.Id as EventId, Events.Name, 
+	(SELECT COUNT(Chips.Id) FROM Chips WHERE Events.Id = FK_EventId) as ConnectedChips,
+	Companies.Id as CompanyId, CompanyName
+	FROM Events
+	INNER JOIN Companies ON FK_CompanyId = Companies.Id
+
+END
