5aa7a7751e0aef03188839929faf023d66a05f39
Ronnie Damsgaard <Ronnie1983@me.com>
2021-11-15 15:05:36 +0100

    Stand name chance and products

---
+++ IdeventLibrary/Models/StandProductModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdeventLibrary.Models
+{
+    public class StandProductModel
+    {
+        public StandProductModel()
+        {
+           
+
+        }
+
+        public StandProductModel(string name, int value, EventStandModel eventStand)
+        {
+            Name = name;
+            Value = value;
+            EventStandModel = eventStand;
+        }
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Value { get; set; }
+        public EventStandModel EventStandModel { get; set; }
+    }
+}
