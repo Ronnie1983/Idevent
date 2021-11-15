f74ef8ea90b0fe19fa24598ea5fa5c65ff59ef85
Ronnie Damsgaard <Ronnie1983@me.com>
2021-11-15 11:17:29 +0100

    EventProductManagement side er opdateret

---
+++ IdeventLibrary/Models/Event.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeventLibrary.Models
+{
+    public class Event
+    {
+        private int _id;
+        private string _name;
+        private Company _company;
+
+        public Event()
+        {
+
+        }
+
+        public Event(string name, Company company)
+        {
+            _name = name;
+            _company = company;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Company Company
+        {
+            get { return _company; }
+            set { _company = value; }
+        }
+    }
+}
---
+++ IdeventLibrary/Models/EventStands.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeventLibrary.Models
+{
+    public class EventStands
+    {
+        private int _id;
+        private string _name;
+        private Event _event;
+        private StandFunctionalities _functionalities;
+
+
+        public EventStands()
+        {
+
+        }
+        public EventStands(string name, Event eventModel, StandFunctionalities functionality)
+        {
+            Name = name;
+            Event = eventModel;
+            Functionalities = functionality;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public Event Event
+        {
+            get { return _event; }
+            set { _event = value; }
+        }
+
+        public StandFunctionalities Functionalities
+        {
+            get { return _functionalities; }
+            set { _functionalities = value; }
+        }
+
+    }
+}
