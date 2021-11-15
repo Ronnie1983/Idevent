1f6e010d85c03c220b41d4d9de05a02cd474962e
Thomas (Noit) <tba@live.dk>
2021-11-15 13:53:33 +0100

    Added Helpers class to retrieve ConnectionString from Environment variables.

--- IdeventAPI/Managers/EventManager.cs
+++ IdeventAPI/Managers/EventManager.cs
@@ -1,14 +1,23 @@
-﻿using IdeventLibrary.Models;
+﻿using Dapper;
+using IdeventLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace IdeventAPI.Managers
 {
     public class EventManager
     {
+        private IDbConnection _dbConnection = new SqlConnection(Helpers.ConnectionString);
+            
         public List<EventModel> GetAll()
         {
             throw new NotImplementedException();
+            string sql = "EXECUTE spGetAllEvents";
+
+            //List<EventModel> events = _dbConnection.Query<EventModel, CompanyModel>(sql);
+
         }
     }
 }
