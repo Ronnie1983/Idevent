1a8c30659eaa1be99b16a58840b6f86c551578f1
Thomas (Noit) <tba@live.dk>
2021-11-16 21:21:47 +0100

    Added comment.

--- IdeventAPI/AppSettings.cs
+++ IdeventAPI/AppSettings.cs
@@ -2,6 +2,10 @@
 
 namespace IdeventAPI
 {
+    /// <summary>
+    /// Should be used to give a connection string to the managers without them having to inject an IConfiguration, but so far I - Noit -
+    /// have not gotten it to work.
+    /// </summary>
     public class AppSettings
     {
         private IConfiguration _config;
