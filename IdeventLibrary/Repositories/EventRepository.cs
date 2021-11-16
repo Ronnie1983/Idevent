ba56e09ebc226f66226ebf48db7fcc17b9a18553
Thomas (Noit) <tba@live.dk>
2021-11-16 10:27:10 +0100

    Added an EventRepository with a GetAllAsync method.

---
+++ IdeventLibrary/Repositories/EventRepository.cs
@@ -0,0 +1,27 @@
+﻿using IdeventLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IdeventLibrary.Repositories
+{
+    public class EventRepository
+    {
+        private static string _baseUrl = "https://localhost:44330/Event/"; // TODO: change to online API
+        private static HttpClient _httpClient = new HttpClient();
+
+        public async Task<List<EventModel>> GetAllAsync()
+        {
+            string jsonContent = await _httpClient.GetStringAsync(new Uri(_baseUrl));
+            var eventList = JsonSerializer.Deserialize<List<EventModel>>(jsonContent);
+
+            return eventList;
+        }
+
+        
+    }
+}
