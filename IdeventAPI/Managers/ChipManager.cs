10576ab86ceebd3593abf743728409f9c537ae30
Thomas (Noit) <tba@live.dk>
2021-11-22 11:52:53 +0100

    Added UserModel.

--- IdeventAPI/Managers/ChipManager.cs
+++ IdeventAPI/Managers/ChipManager.cs
@@ -1,18 +1,31 @@
-﻿using IdeventLibrary.Models;
+﻿using Dapper;
+using IdeventLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace IdeventAPI.Managers
 {
     public class ChipManager
     {
+        IDbConnection _dbConnection;
+
+        ChipManager()
+        {
+            _dbConnection = new SqlConnection(AppSettings.ConnectionString);
+        }
         public List<ChipModel> GetAll()
         {
             return new List<ChipModel>();
         }
         public ChipModel GetById(int id)
         {
-            throw new NotImplementedException();
+            string sql = "EXECUTE spGetChipById @Id";
+
+            ChipModel output = _dbConnection.Query<ChipModel, EventModel, CompanyModel, StandProductModel>(){
+
+            }
         }
     }
 }
---
+++ IdeventLibrary/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeventLibrary.Models
+{
+    public class UserModel : IdentityUser<int>
+    {
+        // IndentityUser<int> properties:
+        // Id, UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumber,
+        // PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount
+
+        public string Role { get; set; }
+        public CompanyModel Company { get; set; }
+        public AddressModel Address { get; set; }
+        public AddressModel InvoiceAddress { get; set; } 
+    }
+}
