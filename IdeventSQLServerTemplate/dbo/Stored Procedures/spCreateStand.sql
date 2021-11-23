fe9a50f13f82049ce5a2c78e25ed9a42f59e1c6b
Ronnie Damsgaard <Ronnie1983@me.com>
2021-11-23 18:16:19 +0100

    UpdateSQL

---
+++ IdeventSQLServerTemplate/dbo/Stored Procedures/spCreateStand.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].spCreateStand
+	@name varchar(50),
+	@eventId int,
+	@funktionalityId int
+AS
+BEGIN
+	INSERT INTO EventStands VALUES (@name, @eventId, @funktionalityId)
+END
---
+++ IdeventSQLServerTemplate/dbo/Stored Procedures/spGetProductsByStandId.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].spGetProductsByStandId
+	@standId int
+AS
+BEGIN
+	SELECT P.Id, P.Name, P.Value 
+	FROM StandProducts AS P
+	WHERE P.FK_EventStandId = @standId
+END
