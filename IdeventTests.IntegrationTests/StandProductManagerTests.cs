054ee058c1b72b4f57857dc43a7b002d738a78b7
Thomas (Noit) <tba@live.dk>
2021-11-25 11:21:22 +0100

    Made test classes fro functionality and product.

---
+++ IdeventTests.IntegrationTests/StandProductManagerTests.cs
@@ -0,0 +1,31 @@
+﻿using IdeventAPI.Managers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeventTests.IntegrationTests
+{
+    [TestClass]
+    public class StandProductManagerTests
+    {
+        private StandProductManager _manager;
+        [TestInitialize]
+        public void Init()
+        {
+            _manager = new StandProductManager();
+        }
+        [TestCleanup]
+        public void CleanUp()
+        {
+
+        }
+        [TestMethod]
+        public void CreateReturnsIdOfCreatedProduct()
+        {
+            _manager.Create
+        }
+    }
+}
