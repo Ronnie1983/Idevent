7524b9f15ab155fff76f64ee574902167fee1ead
Thomas (Noit) <tba@live.dk>
2021-11-29 10:42:24 +0100

    Renamed DummyDataScript folder to Scripts. Made DeleteAllData script. Working on BaseTest to use them.

---
+++ IdeventSQLServerTestDB/dbo/Scripts/Script.DeleteAllTableData.sql
@@ -0,0 +1,10 @@
+﻿-- disable referential integrity
+EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' 
+GO 
+
+EXEC sp_MSForEachTable 'DELETE FROM ?' 
+GO 
+
+-- enable referential integrity again 
+EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL' 
+GO
--- IdeventTests.IntegrationTests/BaseTest.cs
+++ IdeventTests.IntegrationTests/BaseTest.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,17 @@ namespace IdeventTests.IntegrationTests
     public abstract class BaseTest
     {
         public SqlConnection conn = new SqlConnection(TestSettings.ConnectionString);
+        private static string _sqlServerTestProjectRoot = Path.Combine(Directory.GetCurrentDirectory(), "../../../../");
+        private string _scriptFolder = Path.Combine(_sqlServerTestProjectRoot, "IdeventSQLServerTestDB/dbo/Scripts/");
 
         protected abstract void TestSpecificInitialization();
         protected abstract void TestSpecificCleanup();
 
-        [ClassInitialize]
-        public void ClassInit()
-        {
-            // TODO: reset the database on initialization of a test class.
-        }
+        //[ClassInitialize]
+        //public void ClassInit()
+        //{
+           
+        //}
         [ClassCleanup]
         public void ClassCleanup()
         {
@@ -32,11 +35,44 @@ namespace IdeventTests.IntegrationTests
         [TestInitialize]
         public void Init()
         {
+            // TODO: reset the database data on initialization of a test
+            try
+            {
+                string insertScriptPath = Path.Combine(_scriptFolder, "Script.InsertTestData.sql");
+                string[] insertScriptRaw = File.ReadAllLines(insertScriptPath);
+                string insertScript = "";
+                for (int i = 0; i < insertScriptRaw.Length; i++)
+                {
+                    insertScript += insertScriptRaw[i];
+                }
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = insertScript;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+
             TestSpecificInitialization();
         }
         [TestCleanup]
         public void CleanUp()
         {
+            //try
+            //{
+            //    // TODO: Remove all data from database after test.
+            //    SqlCommand cmd = conn.CreateCommand();
+            //    cmd.CommandText = "";
+            //    conn.Open();
+            //    cmd.ExecuteNonQuery();
+            //}
+            //finally
+            //{
+            //    conn.Close();
+            //}
+
             TestSpecificCleanup();
         }
     }
