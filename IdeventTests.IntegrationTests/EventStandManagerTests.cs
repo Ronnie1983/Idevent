f7fc03f73172933da41b5dad537f682678c4c1d7
Thomas (Noit) <tba@live.dk>
2021-11-30 10:27:34 +0100

    Added small check in EventProductManagement for _standFunctionalityCount

---
+++ IdeventTests.IntegrationTests/EventStandManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeventTests.IntegrationTests
+{
+    [TestClass]
+    public class EventStandManagerTests : TestBase
+    {
+        protected override void TestSpecificInitialization()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
