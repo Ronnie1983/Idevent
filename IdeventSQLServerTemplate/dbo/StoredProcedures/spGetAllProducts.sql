3f6e787d8bb096e15d9e5011222761aa168cab7f
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2021-11-30 09:45:09 +0100

    deleted file because it was an old version

--- IdeventSQLServerTemplate/dbo/StoredProcedures/spGetAllProducts.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE PROCEDURE [dbo].spGetAllProducts
-AS
-BEGIN
-	SELECT P.Id, P.Name, P.Value, S.Id, S.Name FROM StandProducts AS P
-	INNER JOIN EventStands AS S ON P.FK_EventStandId = S.FK_EventId
-END
