2747904705ee4268ebc8efcfc4393f41088a1ab4
Thomas (Noit) <tba@live.dk>
2021-12-03 13:13:35 +0100

    Tilføjede JsInteropExtentions, så man kan sætte fokus på et HTML element.

---
+++ IdeventAdminBlazorServer/JsInteropExtentions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace IdeventAdminBlazorServer
+{
+    public static class JsInteropExtentions
+    {
+        public static ValueTask FocusAsync(this IJSRuntime jsRuntime, ElementReference elementReference)
+        {
+            return jsRuntime.InvokeVoidAsync("BlazorFocusElement", elementReference);
+        }
+    }
+}
