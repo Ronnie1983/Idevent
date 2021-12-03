bf814f6ab9c6d7754b16f167b2731375b59ccea5
Thomas (Noit) <tba@live.dk>
2021-12-03 10:42:04 +0100

    Added Update test for CompanyManager (it fails)

--- IdeventTests.IntegrationTests/CompanyManagerTests.cs
+++ IdeventTests.IntegrationTests/CompanyManagerTests.cs
@@ -26,6 +26,50 @@ namespace IdeventTests.IntegrationTests
             Assert.IsInstanceOfType(companies, typeof(List<CompanyModel>));
             Assert.IsTrue(companies.Count >= 0);
         }
+        [TestMethod]
+        public void GetByIdReturnsModel()
+        {
+            CompanyModel company = _manager.GetById(1);
 
+            Assert.IsNotNull(company);
+            Assert.IsInstanceOfType(company, typeof(CompanyModel));
+        }
+        [TestMethod]
+        public void CreateAddsToDatabase()
+        {
+            // TODO: Test CompanyManager.Create();
+            Assert.Fail("Missing Create test.");
+        }
+        [TestMethod]
+        public void UpdateModifiesSpecifiedModel()
+        {
+            CompanyModel oldCompany = _manager.GetById(1);
+            string newEmail = "esdeath@mail.com";
+            AddressModel newAddress = new AddressModel("Trade District 55", "Stormwind", "Eastern Kingdoms", "40K23") { Id = oldCompany.Address.Id};
+            CompanyModel newCompany = new CompanyModel()
+            {
+                Id = oldCompany.Id,
+                Name = oldCompany.Name,
+                Email = newEmail,
+                CVR = oldCompany.CVR,
+                Address = newAddress,
+                InvoiceAddress = oldCompany.InvoiceAddress,
+                PhoneNumber = oldCompany.PhoneNumber,
+                Active = oldCompany.Active,
+                Logo = oldCompany.Logo,
+                Note = oldCompany.Note, 
+            };
+
+            int affectedRows = _manager.UpdateCompany(newCompany);
+            CompanyModel updatedCompany = _manager.GetById(1);
+
+
+            Assert.AreEqual(1, affectedRows);
+            Assert.AreEqual(oldCompany.Id, updatedCompany.Id);
+            Assert.AreEqual(oldCompany.Name, updatedCompany.Name);
+            Assert.AreNotEqual(oldCompany.Email, updatedCompany.Email);
+            Assert.AreNotEqual(oldCompany.Address.StreetAddress, updatedCompany.Address.StreetAddress);
+
+        }
     }
 }
