745709a1053225744bae55a881e7fafe6e8694d5
Thomas (Noit) <tba@live.dk>
2021-12-15 10:33:06 +0100

    Added ids to html elements needed in test.

---
+++ IdeventTests/AddChipTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdeventAdminBlazorServer.Pages.Admin.Chips;
+using Bunit;
+
+namespace IdeventTests
+{
+    [TestClass]
+    public class AddChipTests : TestBase
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+        [TestCleanup]
+        public void Teardown()
+        {
+
+        }
+        [TestMethod]
+        public void SelectCompanyAndOpenEventSelection()
+        {
+            // Arrange
+            IRenderedComponent<AddChip> page = _testContext.RenderComponent<AddChip>(
+
+            // Act
+            var toggleBtn = page.Find("#toggleAddStandBtn");
+            toggleBtn.Click();
+            var addStandContainer = page.Find("#addStandContainer");
+            string addStandContainerMarkup = addStandContainer.ToMarkup();
+
+            // Assert
+            page.Markup.Contains(addStandContainerMarkup);
+
+            // Act
+            toggleBtn.Click();
+
+            // Assert
+            Assert.IsFalse(page.Markup.Contains(addStandContainerMarkup));
+        }
+        [TestMethod]
+        public void SelectEventAndOpenGroupAndProductSelection()
+        {
+            Assert.Fail();
+        }
+    }
+}
