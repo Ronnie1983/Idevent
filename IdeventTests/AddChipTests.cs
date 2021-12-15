8f3a383e4877c863c52130b55eaac7a5c25faa21
Thomas (Noit) <tba@live.dk>
2021-12-15 14:11:05 +0100

    Tried to make a test with bUnit to verify that the next input field showed up.

--- IdeventTests/AddChipTests.cs
+++ IdeventTests/AddChipTests.cs
@@ -6,6 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using IdeventAdminBlazorServer.Pages.Admin.Chips;
 using Bunit;
+using AngleSharp.Dom;
+using Microsoft.Extensions.DependencyInjection;
+using IdeventLibrary.Repositories;
+using IdeventAdminBlazorServer.Shared;
+using IdeventLibrary.Models;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
 
 namespace IdeventTests
 {
@@ -15,38 +22,42 @@ namespace IdeventTests
         [TestInitialize]
         public void Setup()
         {
-
+            _testContext.JSInterop.SetupVoid("BlazorFocusElement", _ => true);
+            _testContext.Services.AddSingleton<CompanyRepository>(new CompanyRepository());
+            _testContext.Services.AddSingleton<ChipRepository>(new ChipRepository());
+            _testContext.Services.AddSingleton<EventRepository>(new EventRepository());
+            _testContext.Services.AddSingleton<ChipGroupRepository>(new ChipGroupRepository());
+            _testContext.Services.AddSingleton<StandProductRepository>(new StandProductRepository());
+            _testContext.Services.AddSingleton<ChipContentRepository>(new ChipContentRepository());
         }
         [TestCleanup]
         public void Teardown()
         {
 
         }
-        [TestMethod]
-        public void SelectCompanyAndOpenEventSelection()
-        {
-            // Arrange
-            IRenderedComponent<AddChip> page = _testContext.RenderComponent<AddChip>(
+        //[TestMethod]
+        //public void SelectCompanyAndShowEventSelection()
+        //{
+        //    // Arrange
+        //    IRenderedComponent<AddChip> page = _testContext.RenderComponent<AddChip>();
+        //    // Give a parameter to the SelectCompany child component on AddChip.
+        //    IRenderedComponent<SelectCompany> selectCompanyComponent = page.FindComponent<SelectCompany>();
+        //    Helpers.SetListParameterForChildComponent<CompanyModel>(selectCompanyComponent, "Companies");
 
-            // Act
-            var toggleBtn = page.Find("#toggleAddStandBtn");
-            toggleBtn.Click();
-            var addStandContainer = page.Find("#addStandContainer");
-            string addStandContainerMarkup = addStandContainer.ToMarkup();
+        //    // Act
+        //    IElement companySelect = page.Find("#companySelect");
+        //    companySelect.Change(1);
+        //    // Arrange
+        //    // Give a parameter to the SelectEvent child component on AddChip.
+        //    IRenderedComponent<SelectEvent> selectEventComponent = page.FindComponent<SelectEvent>();
+        //    Helpers.SetListParameterForChildComponent<EventModel>(selectEventComponent, "Events");
 
-            // Assert
-            page.Markup.Contains(addStandContainerMarkup);
+        //    // Act
+        //    IElement eventSelect = page.Find("#eventSelect");
+        //    string eventSelectMarkup = eventSelect.ToMarkup();
 
-            // Act
-            toggleBtn.Click();
-
-            // Assert
-            Assert.IsFalse(page.Markup.Contains(addStandContainerMarkup));
-        }
-        [TestMethod]
-        public void SelectEventAndOpenGroupAndProductSelection()
-        {
-            Assert.Fail();
-        }
+        //    // Assert
+        //    page.Markup.Contains(eventSelectMarkup);
+        //}
     }
 }
