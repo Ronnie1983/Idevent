cbb2701cf9de79570ba03756754ed9b14878901c
Ronnie Damsgaard <Ronnie1983@me.com>
2021-12-22 11:06:54 +0100

    Change Getallcompanies to left join

--- IdeventSQLServerTemplate/dbo/StoredProcedures/spGetAllCompanies.sql
+++ IdeventSQLServerTemplate/dbo/StoredProcedures/spGetAllCompanies.sql
@@ -3,6 +3,6 @@ AS
 BEGIN
 	SELECT C.id, C.Name, C.CVR, C.PhoneNumber, C.Email, C.CVR, C.Note, A.Id, A.StreetAddress,A.City, A.PostalCode, A.Country, B.Id, B.StreetAddress, B.City, B.PostalCode, B.Country
 	FROM Companies AS C
-	INNER JOIN Addresses as A ON C.AddressId = A.Id
-	INNER JOIN Addresses AS B ON C.InvoiceAddressId = B.Id
+	LEFT JOIN Addresses as A ON C.AddressId = A.Id
+	LEFT JOIN Addresses AS B ON C.InvoiceAddressId = B.Id
 END
